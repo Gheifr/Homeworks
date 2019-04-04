9b833d519161c354f9e4413ff838809a11dcdff3
Gheifr <Gheifr@users.noreply.github.com>
2019-04-04 22:01:22 +0300

    Les 4 fin

---
+++ Lesson 4/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    public class Book
+    {
+        public string Author { get; private set; }
+
+        public string Title;
+
+        private int _numberOfPages;
+        DateTime _dateOfPublication;
+        private string Category;
+
+        public Book(string author, string title)
+        {
+            Author = author;
+            Title = title;
+        }
+
+        public Book(string author)
+        {
+            Author = author;
+        }
+
+        void Read()
+        {
+
+        }
+    }
+}
---
+++ Lesson 4/Librarian.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    class Librarian : Person
+    {
+        Book ProvideBook()
+        {
+            var book = new Book("Ray Bradbery", "Book");
+            book.Title = "Four Seasons";
+            return book;
+        }
+
+    }
+}
