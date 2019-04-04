71686b8358f0157bd58c37082e2aae0fc001c062
Gheifr <Gheifr@users.noreply.github.com>
2019-04-04 19:49:42 +0300

    Les 4 init

--- Project/Program.cs
+++ Project/Program.cs
@@ -10,11 +10,74 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            for (; ; )
-            {
-                Console.WriteLine("bla bla bla");
+            //for (; ; )
+            //{
+            //    Console.WriteLine("bla bla bla");
+            //}
+
+            int StartNum = int.Parse(Console.ReadLine());
+            int EndNum = int.Parse(Console.ReadLine());
+
+            if (StartNum > EndNum)
+            {   
+                return;
             }
 
+            int Sum=0;
+
+            for (int i =StartNum;i<=EndNum;i++)
+            {
+                if (i%2!=0)
+                {
+                    Sum += i;
+                }
+
+            }
+
+            int[] myArray = new int [100];
+            for(int i = 0; i < 100; i++)
+            {
+                myArray[i] = RandomNumber(-100,100);
+            }
+
+            
+            Console.WriteLine("Average num is "+ Average(myArray));
+
+            
+                int maxVal = myArray[0];
+                int minVal = myArray[0];
+
+            for (int i = 0; i <= myArray.Length - 1; i++)
+                {
+                    if (maxVal > myArray[i])
+                    {
+                        maxVal = myArray[i];
+                    }
+                    if (minVal < myArray[i])
+                    {
+                        minVal = myArray[i];
+                    }
+                }
+            Console.WriteLine("Max num is " + maxVal);
+            Console.WriteLine("Min num is " + minVal);
+
+
+            int Average(int [] arr)
+            {
+                int sum=0;
+
+                for (int i=0;i<=arr.Length-1;i++)
+                {
+                    sum += arr[i];
+                }
+                return sum / (arr.Length);
+            }
+
+            int RandomNumber(int min, int max)
+            {
+                Random random = new Random();
+                return random.Next(min, max);
+            }
         }
     }
 }
