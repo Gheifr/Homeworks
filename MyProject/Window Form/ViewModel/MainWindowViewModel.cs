56e12c37803fda654cfddf3b92f17bb3d709b161
Gheifr <Gheifr@users.noreply.github.com>
2019-06-16 15:09:08 +0300

    1

--- MyProject/Window Form/ViewModel/MainWindowViewModel.cs
+++ MyProject/Window Form/ViewModel/MainWindowViewModel.cs
@@ -17,35 +17,92 @@ namespace Window_Form.ViewModel
 {
     public class MainWindowViewModel : PropertyChangedViewModel
     {
+        public delegate void TblChanged(object sender);
+
+        public event TblChanged ActiveTblChanged;
 
         Tbl Table1 = new Tbl(1);
 
         public MainWindowViewModel()
         {
+
             GetTableOrder1 = new RelayCommand(param => HandleGetTableOrder1());
-            ShowOrder = new RelayCommand(param => HandleShowOrder());
+            GetTableOrder2 = new RelayCommand(param => HandleGetTableOrder2());
+            GetTableOrder3 = new RelayCommand(param => HandleGetTableOrder3());
+            GetTableOrder4 = new RelayCommand(param => HandleGetTableOrder4());
+            
+           
+
+
+
         }
 
-        public ObservableCollection<string> HandleShowOrder()
-        {
-            return Table1.Order;
-        }
+       
         
         public void HandleGetTableOrder1()
             {
             if (Table1.Order.Count == 0)
             {
                 //MessageBox.Show($"Bingo! This table number is {Table1.GetTblNum()} \nOrder is: {Table1.GetOrder()}");
-
+                //ToggleButton btn = sender as ToggleButton;
                 Table1.AddOrderItem("Empty by now");
                 Content = Table1.Order;
-                
+                ActiveTblChanged(this);
+
             }
 
             //MessageBox.Show($"Bingo! This table number is {Table1.GetTblNum()} \nOrder is: {Table1.GetOrder()}");
             Content = Table1.Order;
             
             }
+
+       
+
+        public void HandleGetTableOrder4()
+        {
+            if (Table1.Order.Count == 0)
+            {
+                //MessageBox.Show($"Bingo! This table number is {Table1.GetTblNum()} \nOrder is: {Table1.GetOrder()}");
+
+                Table1.AddOrderItem("Empty by now");
+                Content = Table1.Order;
+                ActiveTblChanged(this);
+            }
+
+            //MessageBox.Show($"Bingo! This table number is {Table1.GetTblNum()} \nOrder is: {Table1.GetOrder()}");
+            Content = Table1.Order;
+
+        }
+        public void HandleGetTableOrder2()
+        {
+            if (Table1.Order.Count == 0)
+            {
+                //MessageBox.Show($"Bingo! This table number is {Table1.GetTblNum()} \nOrder is: {Table1.GetOrder()}");
+
+                Table1.AddOrderItem("Empty by now");
+                Content = Table1.Order;
+                ActiveTblChanged(this);
+            }
+
+            //MessageBox.Show($"Bingo! This table number is {Table1.GetTblNum()} \nOrder is: {Table1.GetOrder()}");
+            Content = Table1.Order;
+
+        }
+        public void HandleGetTableOrder3()
+        {
+            if (Table1.Order.Count == 0)
+            {
+                //MessageBox.Show($"Bingo! This table number is {Table1.GetTblNum()} \nOrder is: {Table1.GetOrder()}");
+
+                Table1.AddOrderItem("Empty by now");
+                Content = Table1.Order;
+                ActiveTblChanged(this);
+            }
+
+            //MessageBox.Show($"Bingo! This table number is {Table1.GetTblNum()} \nOrder is: {Table1.GetOrder()}");
+            Content = Table1.Order;
+
+        }
 
         private ObservableCollection<string> _orderText;
         public ObservableCollection<string> Content
@@ -62,8 +119,10 @@ namespace Window_Form.ViewModel
         }
 
         public ICommand GetTableOrder1 { get; private set; }
-        public ICommand ShowOrder { get; private set; }
-
+        public ICommand GetTableOrder2 { get; private set; }
+        public ICommand GetTableOrder3 { get; private set; }
+        public ICommand GetTableOrder4 { get; private set; }
+        
         
     }
     
