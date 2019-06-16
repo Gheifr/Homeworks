e54e7ee1fca03e0537ced29cd45089e39b538a61
Gheifr <Gheifr@users.noreply.github.com>
2019-06-16 10:43:53 +0300

    Matrix

---
+++ MAtrix/IMatrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3L2
+{
+    public interface IMatrix<T>:IEnumerable<T>
+    {
+
+        
+        T GetItem(int x, int y);
+        //T GetItem(string name);
+        void Insert(int x, int y, T item);
+
+        int GetCount();
+
+    }
+}
